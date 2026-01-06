f6d735764301d72ff8314d82b7bd63a7d9c8d0c8
parameswaran123597 <paramu920@gmail.com>
2026-01-06 15:20:52 +0530

    Add files via upload

---
+++ 20260106-day6clswrk.py
@@ -0,0 +1,19 @@
+attendance=[18,20,19,15,21]
+i=0
+total_attendence=0
+for x in attendance:
+    if x>=20:
+        print("Full")
+        i=i+1
+    else:
+        print("Not Full")
+      
+total_attendence=total_attendence+x
+print("total attendance is :",total_attendence)
+print("No of full days is :",i)
+        
+        
+        
+        
+        
+            
