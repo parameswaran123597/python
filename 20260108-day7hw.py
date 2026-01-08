dc6a4e5eada46d19cd57cd20619cacac33864890
parameswaran123597 <paramu920@gmail.com>
2026-01-08 13:34:54 +0530

    Add files via upload

---
+++ 20260108-day7hw.py
@@ -0,0 +1,24 @@
+inventory=[]
+def add_item(item):
+    inventory.append(item)
+    print(inventory)
+    
+def count_items(items):
+    if items == []:
+        return 0
+    return 1+count_items(items[1:])
+    
+def main():   
+    add_item("dog food")   
+    add_item("cat toy")
+    add_item("bird cage") 
+    add_item("fish tank")
+    
+    show_item=lambda item:print(f"Item in Stock: {item}")
+    
+    for item in inventory:
+        show_item(item)
+    
+    print("Total number of items =",count_items(inventory))
+  
+main()    
